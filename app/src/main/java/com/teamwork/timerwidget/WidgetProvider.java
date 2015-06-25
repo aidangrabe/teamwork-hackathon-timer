610e4804a53d7b0effb7583b4b28f4ebd22a46af
Aidan Grabe <aidan@teamwork.com>
2015-06-25 10:42:12 +0100

    Initial layout

--- app/src/main/java/com/teamwork/timerwidget/WidgetProvider.java
+++ app/src/main/java/com/teamwork/timerwidget/WidgetProvider.java
@@ -3,6 +3,7 @@ package com.teamwork.timerwidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.RemoteViews;
 
 /**
  * Created by aidangrabe on 25/06/15.
@@ -17,6 +18,15 @@ public class WidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int widgetId : appWidgetIds) {
+
+            RemoteViews remoteViews = new RemoteViews(
+                    context.getPackageName(),R.layout.widget_layout);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
+        }
     }
 
 }
