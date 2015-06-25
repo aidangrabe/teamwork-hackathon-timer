4ab72523bb4cd70585030597e262da8a75bafb8c
Aidan Grabe <aidan@teamwork.com>
2015-06-25 10:08:51 +0100

    First commit

---
+++ app/src/main/java/com/teamwork/timerwidget/WidgetProvider.java
@@ -0,0 +1,22 @@
+package com.teamwork.timerwidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by aidangrabe on 25/06/15.
+ *
+ */
+public class WidgetProvider extends AppWidgetProvider {
+
+    public WidgetProvider() {
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+}
